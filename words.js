dcbdc4275cde687a28f235a91448b89ec1a82f97
Lesego864 <tshipulesego@gmail.com>
2022-03-22 17:03:23 +0200

    made the longest word function

--- words.js
+++ words.js
@@ -17,10 +17,12 @@ let sentenceButton = () => {
         if (elem.length > 4) {
             outputElem.innerHTML += `<mark style = "background: lightblue ">` + elem + `</mark>` + " ";
 
+
         } else {
             outputElem.innerHTML += elem + " ";
         }
         wordCount.innerHTML = `There are <strong> ${words.length} </strong> words in a sentence.`
+        console.log(words);
     }
 }
 
@@ -34,7 +36,8 @@ let checkButton = () => {
         for (let i = 0; i < words.length; i++) {
             let elem = words[i]
             if (elem.length >= 5) {
-                sent += `<mark>${elem} </mark> `
+                sent += `<mark style>${elem} </mark> `;
+                // sent += `<span style="display: none">${elem} </span> `;
             }
         }
         outputElem.innerHTML = sent;
@@ -42,30 +45,38 @@ let checkButton = () => {
         outputElem.innerHTML = " ";
         sentenceButton();
     }
-}
+};
+// let longestWord = () => {
+//     length: 0
+// };
 
 let longest = () => {
     const wordVal = wordInputElem.value;
-    var longestWord = " ";
     let words = wordVal.split(" ");
+    var longestWord = " ";
     let sent = " ";
     for (let i = 0; i < words.length; i++) {
         let elem = words[i];
-        if ((hideWordsElem.checked == true) && (longestWord < words[i].length)) {
+        // if ((hideWordsElem.checked == true) && (longestWord < elem)) {
+        //     longestWord = elem;
+        //     // sent += `<span style="display: none">${longestWord} </span> `;
+        // sent += `<mark style= background: green>${longestWord} </mark>`;
+        //     sent += `<mark style= background: #CB997E>${longestWord} </mark>`;
+        // } else {
+        //     sent += " ";
+        // }
+        if (elem.length > longestWord.length) {
             longestWord = elem;
-            // sent += `<span style="display: none">${longestWord} </span> `;
-
-            sent += `<mark style= background: #CB997E>${longestWord} </mark>`;
-        } else {
-            sent += " ";
+            messageElem.innerHTML = `The longest word is: <mark style = "background: lightgreen">${longestWord}</mark> `
         }
-
-        messageElem.innerHTML = `The longest word is ${longestWord}`;
-
+        // messageElem.innerHTML = `The longest word is ${longestWord}`;
     }
-    messageElem.innerHTML = sent;
+    console.log(longestWord);
+
+    // messageElem.innerHTML = sent;
 
 }
+
 
 hideWordsElem.addEventListener('click', function() {
     checkButton();
