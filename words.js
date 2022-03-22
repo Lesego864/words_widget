93151a779625586d96914b0e801b0f6e7a955635
Lesego864 <tshipulesego@gmail.com>
2022-03-22 08:05:17 +0200

    attempt on the longest word, still unable to to make it function

--- words.js
+++ words.js
@@ -10,18 +10,18 @@ const hideWordsElem = document.querySelector('.hide');
 let sentenceButton = () => {
     const wordVal = wordInputElem.value;
     let words = wordVal.split(" ");
+
+
     for (let i = 0; i < words.length; i++) {
         let elem = words[i];
-
         if (elem.length > 4) {
             outputElem.innerHTML += `<mark style = "background: lightblue ">` + elem + `</mark>` + " ";
+
         } else {
             outputElem.innerHTML += elem + " ";
         }
         wordCount.innerHTML = `There are <strong> ${words.length} </strong> words in a sentence.`
     }
-
-
 }
 
 analyzeBtn.addEventListener('click', sentenceButton);
@@ -44,5 +44,30 @@ let checkButton = () => {
     }
 }
 
+let longest = () => {
+    const wordVal = wordInputElem.value;
+    var longestWord = " ";
+    let words = wordVal.split(" ");
+    let sent = " ";
+    for (let i = 0; i < words.length; i++) {
+        let elem = words[i];
+        if ((hideWordsElem.checked == true) && (longestWord < words[i].length)) {
+            longestWord = elem;
+            // sent += `<span style="display: none">${longestWord} </span> `;
 
-hideWordsElem.addEventListener('click', checkButton);
+            sent += `<mark style= background: #CB997E>${longestWord} </mark>`;
+        } else {
+            sent += " ";
+        }
+
+        messageElem.innerHTML = `The longest word is ${longestWord}`;
+
+    }
+    messageElem.innerHTML = sent;
+
+}
+
+hideWordsElem.addEventListener('click', function() {
+    checkButton();
+    longest();
+});
